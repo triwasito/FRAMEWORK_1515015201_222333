5491a463f3246ece473ada84b71ebf428f946e59
Tri Wasito <triwasito23@gmail.com>
2017-04-22 10:26:39 +0700

    post test 7

--- resources/views/ruangan/form.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-	<label class="col-sm-2 control-label">Title</label>
-	<div class="col-sm-10">
-	{!! Form::text('title',null,['class'=>'form-control','placeholder'=>"Title"]) !!}
-	</div>
-</div>
-
- 
